593edc65b84c75e591c02b1d1a5d7a03b63bad3a
Michael Liu <liumichaeler@gmail.com>
2025-06-05 21:23:15 -0400

    Created structure and cleaning function

--- database_gdp/database.cpp
+++ database_gdp/database.cpp
@@ -5,7 +5,7 @@
 #include <string>
 using namespace std;
 
-struct StateData { //Object structure for the dictionary
+struct StateData { //Structure for the dictionary
         double gdp_per_worker;
         double workforce_in_stem;
         double urbanization_rate;
@@ -14,16 +14,21 @@ struct StateData { //Object structure for the dictionary
         double true_value_USD;
     };
 
-string clean(const string & s) { //Cleaning function for data in the .txt file
-    string result; //Any character that isnt a digit or decimal point is removed
-    for (char c : s) {
-        if (isdigit(c) || c == '.') result += c;
-    }
-    return result;
+string clean(string input) { //Cleaning function for data in the .txt file
+    string cleaned_string = ""; //Any character that isn't a digit or decimal point is removed
+    
+    for(int i = 0; i < input.length(); i++){
+        char current_char = input[i];
+        if (isdigit(current_char) || current_char == '.'){ //If the current char's ASCII value is between 0 and 9's ASCII value or if its a decimal
+            cleaned_string += current_char;
+        };
+    };
+    return cleaned_string;
 };
 
 int main() {
     ifstream file("merged_gdp_data.txt"); //opens the file for reading
-    map <string, StateData> gdp_database = {
-        
+    map <string, StateData> gdp_database; //dictionary for storing values
+    
+
 };
