96f0ff18e28b7a0aa96de6dac19f1fb3a37d6ef3
Michael Liu <liumichaeler@gmail.com>
2025-06-06 20:14:55 -0400

    Rename

--- database_gdp/csv_to_txt.py
+++
@@ -1,3 +0,0 @@
-with open("merged_gdp_data.csv", "r") as csv_file, open("merged_gdp_data.txt", "w") as txt_file:
-    for line in csv_file:
-        txt_file.write(line.replace(",", " "))
--- hello_world.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, World" << endl; //cout to output and endl to end line
-}
