b1b162bee0f868beb4eb0af511ceb4bbf78c4a58
Michael Liu <liumichaeler@gmail.com>
2025-06-09 21:58:45 -0400

    Created snake

--- 7_oop_snake_game/main.cpp
+++ 7_oop_snake_game/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <raylib.h>
+#include <deque>
 
 using namespace std;
 
@@ -8,6 +9,20 @@ Color dark_green = {43, 51, 24, 255}; //GUI Colors
 
 int cell_size = 30;
 int cell_count = 25; //Used to create an invisible grid
+
+class Snake { //The snake will be represented as a deque, where elements can be easily removed or added in front and back like a vector array
+    public:
+        deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
+
+        void Draw() { //Draw the snake using rounded rectangles
+            for(unsigned int i = 0; i < body.size(); i++) {
+                float x = body[i].x;
+                float y = body[i].y;
+                Rectangle segment = Rectangle(x * cell_size, y * cell_size, (float)cell_size, (float)cell_size);
+                DrawRectangleRounded(segment, 0.5, 6, dark_green);
+            }
+        }
+};
 
 class Food { //Class to represent the snake food
     public:
@@ -46,6 +61,7 @@ int main() {
     SetTargetFPS(60); //60 Frames per second
 
     Food food = Food();
+    Snake snake = Snake();
     
     //Game Loop Components -> 1: Event Handling, 2: Updating Postitions, 3: Drawing Objects
     while(WindowShouldClose() == false) {
@@ -53,6 +69,7 @@ int main() {
 
         //Drawing
         ClearBackground(green);
+        snake.Draw();
         food.Draw();
 
         EndDrawing();
