0f8b898ac848822dc78e29e1ee810c85ecdac77f
Michael Liu <liumichaeler@gmail.com>
2025-06-13 11:48:26 -0400

    In class coding task

---
+++ 8_pascals_triangle/pascals_triangle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter the number of Pascal Triangle rows--> " << endl;
+    int rows;
+    cin >> rows;
+    int triangle[100][100] = {0}; //Fill with all 0's, don't print the 0's later
+
+    for(int i = 0; i < rows + 1; i++) {
+        triangle[i][0] = 1; //First element 1
+        triangle[i][i] = 1; //Last element 1
+
+        for(int j = 1; j < i; j++) { 
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]; //Add number above to the left and just above
+        }
+    }
+
+    for(int k = 0; k < rows; k++) {
+        for(int n = 0; n < rows; n++) {
+            if (triangle[k][n] != 0) {
+            cout << triangle[k][n] << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
